db8a165b2e8bbdee469d347cc7f410bf21f147e0
Jonathan Boler <galatians@gmail.com>
2015-12-30 12:59:29 -0600

    Add base_path option to include asset methods

--- app/helpers/ember_cli_rails_assets_helper.rb
+++ app/helpers/ember_cli_rails_assets_helper.rb
@@ -1,23 +1,23 @@
 require "ember_cli/assets/lookup"
 
 module EmberCliRailsAssetsHelper
-  def include_ember_script_tags(name)
+  def include_ember_script_tags(name, base_path=nil)
     EmberCli[name].build
 
     assets = EmberCli::Assets::Lookup.new(EmberCli[name])
 
     assets.javascript_assets.
-      map { |src| %{<script src="#{src}"></script>}.html_safe }.
+      map { |src| %{<script src="#{base_path}#{src}"></script>}.html_safe }.
       inject(&:+)
   end
 
-  def include_ember_stylesheet_tags(name)
+  def include_ember_stylesheet_tags(name, base_path=nil)
     EmberCli[name].build
 
     assets = EmberCli::Assets::Lookup.new(EmberCli[name])
 
     assets.stylesheet_assets.
-      map { |src| %{<link rel="stylesheet" href="#{src}">}.html_safe }.
+      map { |src| %{<link rel="stylesheet" href="#{base_path}#{src}">}.html_safe }.
       inject(&:+)
   end
 end

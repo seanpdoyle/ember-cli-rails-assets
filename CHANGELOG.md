ca57316d9e68d75be83c161cdc70951c3db0a7d5
Sean Doyle <sean.p.doyle24@gmail.com>
2016-01-15 15:49:50 -0500

    Remove `mount_ember_assets`
    This functionality is provided by
      [`ember-cli-rails`][ember-cli-rails#386].
    
    [ember-cli-rails#386]: https://github.com/thoughtbot/ember-cli-rails/pull/386

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,11 @@
 master
 ------
 
+* Remove `mount_ember_assets`. This is handled by
+  [`ember-cli-rails`][ember-cli-rails#386].
+
+[ember-cli-rails#386]: https://github.com/thoughtbot/ember-cli-rails/pull/386
+
 0.6.1
 -----
 
--- lib/ember_cli/assets/engine.rb
+++ lib/ember_cli/assets/engine.rb
@@ -2,7 +2,6 @@ module EmberCli
   module Assets
     class Engine < ::Rails::Engine
       initializer "ember-cli-rails-assets" do
-        require "ember_cli/assets/ext/action_dispatch/routing/mapper"
         ActionController::Base.helper EmberCliRailsAssetsHelper
       end
     end

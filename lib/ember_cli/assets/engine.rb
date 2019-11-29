1135c767f36ab3bee3f35c96bae2288875f83066
Bradley Priest <bradleypriest@gmail.com>
2019-11-29 18:54:32 +0800

    Update engine.rb to work with zeitwerk

--- lib/ember_cli/assets/engine.rb
+++ lib/ember_cli/assets/engine.rb
@@ -2,7 +2,9 @@ module EmberCli
   module Assets
     class Engine < ::Rails::Engine
       initializer "ember-cli-rails-assets" do
-        ActionController::Base.helper EmberCliRailsAssetsHelper
+        ActiveSupport.on_load(:action_controller) do
+          ActionController::Base.helper EmberCliRailsAssetsHelper
+        end
       end
     end
   end
